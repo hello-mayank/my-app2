20ffe82f9e212d585f985daf410180266c517030
Mayank Yadav <mayankyadav0832@gmail.com>
2026-04-09 08:27:01 +0000

    fix line app.py

--- app/app.py
+++ app/app.py
@@ -1,15 +1,19 @@
 from flask import Flask, jsonify
 import os
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return jsonify(message="Hello DevOps Engg")
 
+
 @app.route("/health")
 def health():
     return jsonify(status="OK")
+
 
 if __name__ == "__main__":
     env = os.getenv("ENV", "development")
